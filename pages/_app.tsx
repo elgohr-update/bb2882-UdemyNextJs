4a761aead4b1772883e3c3f9761a4d310836b60d
Azat <harutyunyanazat29@gmail.com>
2022-07-18 21:51:58 +0400

    Htag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
+import Head from 'next/head';
 import { AppProps } from '../node_modules/next/app';
 import '../styles/globals.css';
+import React from 'react';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+	return <>
+		<Head>
+			<title>MyTop</title>
+			<link rel="icon" href="/favicon.ico" />
+			<link rel="preconnect" href="https://fonts.googleapis.com"/>
+			<link rel="preconnect" href="https://fonts.gstatic.com"/>
+			<link href="https://fonts.googleapis.com/css2?family=Merienda&family=Noto+Sans+Display:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
+		</Head>
+		<Component {...pageProps} />
+	</>;
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document'
+
+class MyDocument extends Document {
+	static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+		const initalProps = await Document.getInitialProps(ctx);
+		return {...initalProps};
+	}
+
+	render(): JSX.Element {
+		return (
+			<Html lang="ru">
+				<Head/>
+				<body>
+					<Main/>
+					<NextScript/>
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;
