67dee5ae6cc815f48bbd38c5955c2e5bc27be7d6
Azat <harutyunyanazat29@gmail.com>
2022-07-18 23:39:24 +0400

    Button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import { Htag } from '../components';
+import { Button, Htag } from '../components';
 
 export default function Home(): JSX.Element {
 	return (
 		<div>
 			<Htag tag='h1'>Text</Htag>
+			<Button appearance='primary'>Button</Button>
+			<Button appearance='ghost'>Button</Button>
 		</div>
 	);
 }
